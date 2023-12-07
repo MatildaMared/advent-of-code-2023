054492c8d7a70fb0cdda6b8c47a0651c869c5fe2
Matilda Mared <matildamared@live.se>
2023-12-07 06:08:52 +0100

    prepare for day 07

---
+++ 07/index.ts
@@ -0,0 +1,20 @@
+import * as fs from "fs";
+import * as path from "path";
+import { Race, calculateNumberOfPossibleWins } from "./utils";
+
+// 🎄 🎅 Advent of Code 2023 Day 7 🎅 🎄
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+const line = input
+  .split("\n");
+
+// 🍬 🍭 Part 1 🍭 🍬
+
+
+console.log(`🎄 🎅 The answer to part 1 is: ${undefined} 🍬 🍭`);
+
+// 🍬 🍭 Part 2 🍭 🍬
+
+console.log(
+	`🎄 🎅 The answer to part 2 is: ${undefined} 🍬 🍭`
+);
