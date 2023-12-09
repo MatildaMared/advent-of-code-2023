5128f30abdd27788b11b38c0cbca05190823e83e
Matilda Mared <matildamared@live.se>
2023-12-09 06:40:46 +0100

    prepare for day 09

---
+++ 09/index.ts
@@ -0,0 +1,11 @@
+import * as fs from "fs";
+import * as path from "path";
+
+// 🎄 🎅 Advent of Code 2023 Day 9 🎅 🎄
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+const line = input.split("\n");
+
+// 🍬 🍭 Part 1 🍭 🍬
+
+// 🍬 🍭 Part 1 🍭 🍬
