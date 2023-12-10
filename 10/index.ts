4e18593e6bd50b8ecd6afe9b7bf4530506b2297b
Matilda Mared <matildamared@live.se>
2023-12-10 06:28:48 +0100

    prepare for day 10

---
+++ 10/index.ts
@@ -0,0 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
+
+// 🎄 🎅 Advent of Code 2023 Day 10 🎅 🎄
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+const lines = input.split("\n");
+
+// 🍬 🍭 Part 1 🍭 🍬
+
+console.log(`🎄 🎅 The answer to part 1 is: ${undefined} 🍬 🍭`);
+
+// 🍬 🍭 Part 2 🍭 🍬
+
+console.log(`🎄 🎅 The answer to part 2 is: ${undefined} 🍬 🍭`);
