58d497057d37f5f60959714693b38dfc76f4281b
Matilda Mared <matildamared@live.se>
2023-12-21 15:26:36 +0100

    prepare for day 21

---
+++ 21/index.ts
@@ -0,0 +1,12 @@
+import * as fs from "fs";
+import * as path from "path";
+
+// 🎄 🎅 Advent of Code 2023 Day 21 🎅 🎄
+
+// 🍬 🍭 Part 1 🍭 🍬
+
+console.log(`🎄 🎅 The answer to part 1 is: ${null} 🍬 🍭`);
+
+// 🍬 🍭 Part 2 🍭 🍬
+
+console.log(`🎄 🎅 The answer to part 2 is: ${null} 🍬 🍭`);
